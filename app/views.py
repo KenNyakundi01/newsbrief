6179217c3c9d836d19b9d4dbdb4e87a748b61b32
Kenn <sabatojnr01@gmail.com>
2020-07-06 10:20:52 +0300

    worked on getting popular news

--- app/views.py
+++ app/views.py
@@ -1,25 +1,25 @@
 from app import app
 from .requests import get_news,get_news,search_news
 from flask import render_template,request,redirect,url_for
-# Views
- 
-@app.route('/news/<news_id>')
-def news(news_id):
+
+    # Views
+@app.route('/')
+def index():
 
     '''
-    View news page function that returns the news details page and its data
+    View root page function that returns the index page and its data
     '''
-    return render_tesplate('news.html',id = news_id)
 
+    # Getting popular news
+    trending_news = get_news('trending')
+    highlights_news = get_news('highlights')
+    live_news = get_news('live')
 
+    title = 'Home - You can view the latest news'
 
-    @app.route('/search/<news_name>')
-def search(news_name):
-    '''
-    View function to display the search results
-    '''
-    news_name_list =news_name.split(" ")
-    news_name_format = "+".join(news_name_list)
-    searched_news = search_news(news_name_format)
-    title = f'search results for {news_name}'
-    return render_template('search.html',news = searched_news)
+    search_news = request.args.get('news_query')
+
+    if search_news:
+        return redirect(url_for('search',news_name=search_news))
+    else:
+        return render_template('index.html', title = title, popular = trending_news, highlights = highlights_news, live = live_news )
